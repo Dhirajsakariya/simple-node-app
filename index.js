5cb2144e82c1c2ad8544be17405c0cff76897397
Dhiraj <dhirajsakariya@gmail.com>
2019-06-12 18:02:11 +0530

    changed port to 3000

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require("http");
 
-const port = process.env.port || 3002;
+const port = process.env.port || 3000;
 
 const handler =(req,res) => {
     console.log("Server received request ");
